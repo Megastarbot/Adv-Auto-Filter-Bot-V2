cf2448e528267eef4c3669d34b03102238808344
Megastarbot <89236767+Megastarbot@users.noreply.github.com>
2021-09-12 12:42:21 +0530

    Upi info spy

--- bot/translation.py
+++ bot/translation.py
@@ -14,11 +14,12 @@ For More Details Click Help Button Below..
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b> ഒന്ന് പോടാ അവൻ help ചോദിച്ചു
+വനേൽക്കുന്നു you cnow iam difrrend bot
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+-> 
+-> 
 </i>
 
 <b>Bot Commands (Works Only In Groups) :</b>
@@ -48,11 +49,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ devaleppar</b> : <b><i><a href="https://</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/ajukuttan">Click Me</a></i>
 """
